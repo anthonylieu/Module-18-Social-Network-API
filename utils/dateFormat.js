3b9187f42e253590c2befc545d6fd2efa12e16f3
anthonylieu <38957648+anthonylieu@users.noreply.github.com>
2023-05-02 03:26:12 -0400

    updated dateFormat

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -1,14 +1,15 @@
+// Function to add appropriate suffix to the date
 const addDateSuffix = (date) => {
   let dateStr = date.toString();
 
-  // get last char of date string
+  // Get last character of date string
   const lastChar = dateStr.charAt(dateStr.length - 1);
 
-  if (lastChar === '1' && dateStr !== '11') {
+  if (lastChar === "1" && dateStr !== "11") {
     dateStr = `${dateStr}st`;
-  } else if (lastChar === '2' && dateStr !== '12') {
+  } else if (lastChar === "2" && dateStr !== "12") {
     dateStr = `${dateStr}nd`;
-  } else if (lastChar === '3' && dateStr !== '13') {
+  } else if (lastChar === "3" && dateStr !== "13") {
     dateStr = `${dateStr}rd`;
   } else {
     dateStr = `${dateStr}th`;
@@ -17,42 +18,43 @@ const addDateSuffix = (date) => {
   return dateStr;
 };
 
-// function to format a timestamp, accepts the timestamp and an `options` object as optional parameters
+// Function to format a timestamp, accepts the timestamp and an `options` object as optional parameters
 module.exports = (
   timestamp,
-  { monthLength = 'short', dateSuffix = true } = {}
+  { monthLength = "short", dateSuffix = true } = {}
 ) => {
   let months;
 
-  if (monthLength === 'short') {
+  // Set short or long month names based on monthLength parameter
+  if (monthLength === "short") {
     months = {
-      0: 'Jan',
-      1: 'Feb',
-      2: 'Mar',
-      3: 'Apr',
-      4: 'May',
-      5: 'Jun',
-      6: 'Jul',
-      7: 'Aug',
-      8: 'Sep',
-      9: 'Oct',
-      10: 'Nov',
-      11: 'Dec',
+      0: "Jan",
+      1: "Feb",
+      2: "Mar",
+      3: "Apr",
+      4: "May",
+      5: "Jun",
+      6: "Jul",
+      7: "Aug",
+      8: "Sep",
+      9: "Oct",
+      10: "Nov",
+      11: "Dec",
     };
   } else {
     months = {
-      0: 'January',
-      1: 'February',
-      2: 'March',
-      3: 'April',
-      4: 'May',
-      5: 'June',
-      6: 'July',
-      7: 'August',
-      8: 'September',
-      9: 'October',
-      10: 'November',
-      11: 'December',
+      0: "January",
+      1: "February",
+      2: "March",
+      3: "April",
+      4: "May",
+      5: "June",
+      6: "July",
+      7: "August",
+      8: "September",
+      9: "October",
+      10: "November",
+      11: "December",
     };
   }
 
@@ -61,6 +63,7 @@ module.exports = (
 
   let dayOfMonth;
 
+  // Add date suffix if dateSuffix parameter is true
   if (dateSuffix) {
     dayOfMonth = addDateSuffix(dateObj.getDate());
   } else {
@@ -70,28 +73,29 @@ module.exports = (
   const year = dateObj.getFullYear();
 
   let hour;
-  // check for 24-hr time
+  // Convert to 12-hour time if hours are greater than 12
   if (dateObj.getHours > 12) {
     hour = Math.floor(dateObj.getHours() / 2);
   } else {
     hour = dateObj.getHours();
   }
-  // if hour is 0 (12:00am), change it to 12
+  // If hour is 0 (12:00am), change it to 12
   if (hour === 0) {
     hour = 12;
   }
 
   const minutes = dateObj.getMinutes();
 
-  // set `am` or `pm`
+  // Set 'am' or 'pm' based on the hours
   let periodOfDay;
 
   if (dateObj.getHours() >= 12) {
-    periodOfDay = 'pm';
+    periodOfDay = "pm";
   } else {
-    periodOfDay = 'am';
+    periodOfDay = "am";
   }
 
+  // Combine all formatted elements into a single formatted timestamp string
   const formattedTimeStamp = `${formattedMonth} ${dayOfMonth}, ${year} at ${hour}:${minutes} ${periodOfDay}`;
 
   return formattedTimeStamp;
